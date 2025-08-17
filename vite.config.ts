388e753d2d58a7be3073126ad3118bf3656d9867
webster-rafael <webster.rafael2014@gmail.com>
2025-08-17 19:37:17 -0400

    att vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 3000,
-    watch: {
-      usePolling: true,
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
